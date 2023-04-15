5f663c9cea7bf8554002b0c11e38f7e7d0814c69
Navaneeth Arunkumar <navaarun18@gmail.com>
2023-04-15 11:39:51 +0530

    Carousel added to company listing page

---
+++ Frontend/src/components/Carousel/Data.js
@@ -0,0 +1,18 @@
+import image1 from "./images/img1.png";
+import image2 from "./images/img2.png";
+import image3 from "./images/img3.png";
+
+export const pictures = [
+    {
+        Pictures: image1,
+        title: 'A'
+    },
+    {
+        Pictures: image2,
+        title: 'B'
+    },
+    {
+        Pictures: image3,
+        title: 'C'
+    }
+]
